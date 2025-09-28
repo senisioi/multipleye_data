3f3cf2447f23cd718886da20f43e76e5bfcff376
Anton Marius-Alexandru <92689316+marius-anton142@users.noreply.github.com>
2025-09-28 09:03:18 +0300

    Changed data/ to data/processed in get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,9 @@
-#/bin/bash
+#!/bin/bash
 
-mkdir -p data && \
-cd data && \
+mkdir -p data/processed && \
+cd data/processed && \
 rm -rf csv* && \
-wget https://github.com/senisioi/multipleye_data/releases/download/0.1/csv.tar.gz \\
+wget https://github.com/senisioi/multipleye_data/releases/download/0.1/csv.tar.gz && \
 tar -xvf csv.tar.gz && \
-mv csv/* .
+mv csv/* . && \
+rmdir csv
